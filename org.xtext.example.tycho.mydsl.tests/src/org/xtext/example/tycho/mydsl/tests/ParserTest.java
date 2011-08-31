6e46826fd7f0a2ce24d9095b3c041db61e185c8e
Dennis Huebner <dennis.huebner@itemis.de>
2011-08-31 15:48:01 +0200

    ready to use - build success TODO add source bundles, use local eclipse as target platform

---
+++ org.xtext.example.tycho.mydsl.tests/src/org/xtext/example/tycho/mydsl/tests/ParserTest.java
@@ -0,0 +1,41 @@
+package org.xtext.example.tycho.mydsl.tests;
+
+import static junit.framework.Assert.*;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.eclipse.xtext.junit4.util.ParseHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.xtext.example.mydsl.MyDslInjectorProvider;
+import org.xtext.example.mydsl.MyDslUiInjectorProvider;
+import org.xtext.example.mydsl.myDsl.Greeting;
+import org.xtext.example.mydsl.myDsl.Model;
+
+import com.google.inject.Inject;
+
+/**
+ * Simple Parser non UI Test.<br>
+ * 
+ * @see MyDslInjectorProvider
+ * @see MyDslUiInjectorProvider
+ * 
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(MyDslInjectorProvider.class)
+public class ParserTest {
+
+	@Inject
+	private ParseHelper<EObject> parseHelper;
+
+	@Test
+	public void testParser() throws Exception {
+		EObject model = parseHelper.parse("Hello world!");
+		assertTrue(model instanceof Model);
+		EList<Greeting> greetings = ((Model) model).getGreetings();
+		assertEquals(1, greetings.size());
+		assertEquals("world", greetings.get(0).getName());
+	}
+}
